90bde358a79fa4ef5a2640702c28133302caee5f
scyyh11 <yizhanhuang2002@gmail.com>
2023-07-20 20:02:01 +0800

    deleted:    Assets/Collision.cs 	new file:   Assets/Delivery.cs 	renamed:    Assets/Collision.cs.meta -> Assets/Delivery.cs.meta 	modified:   Assets/Driver.cs 	new file:   Assets/FollowCamera.cs 	new file:   Assets/FollowCamera.cs.meta 	modified:   Assets/Scenes/SampleScene.unity 	modified:   ProjectSettings/TagManager.asset

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,14 +5,10 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
 
-    [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float steerSpeed = 300f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 10f;
+    [SerializeField] float boostSpeed = 30f;
 
     // Update is called once per frame
     void Update()
@@ -23,5 +19,19 @@ public class Driver : MonoBehaviour
 
         transform.Rotate(0,0,-steerAmount);
         transform.Translate(0,moveAmount,0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag=="Boost")
+        {
+            Debug.Log("Speed up");
+            moveSpeed=boostSpeed;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        moveSpeed=slowSpeed;
     }
 }
